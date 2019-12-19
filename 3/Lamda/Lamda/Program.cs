0b99de5eed89c800bb310e9c9598dcf5e872e8a9
albert18 <erertech@gmail.com>
2019-12-18 20:21:03 -0500

    info lambda expression

--- 3/Lamda/Lamda/Program.cs
+++ 3/Lamda/Lamda/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lamda
 {
+    public delegate int MyDelegate(int x);
+    public delegate void MyDelegate2(int x, string prefix);
+    public delegate bool ExprDelegate (int x);
+
     class Program
     {
         static void Main(string[] args)
         {
+            //Dynamic change lambda expression
+            MyDelegate foo = (x) => x * x;
+            Console.WriteLine("result {0}", foo(5));
+
+            //statement lambda
+            MyDelegate2 bar = (x, y) =>
+            {
+                Console.WriteLine("{0}, {1}", x * 10, y);
+            };
+            bar(25, "Some String");
+
+            //Define expression delegate
+            ExprDelegate baz = (x) => x > 10;
+            Console.WriteLine("{0}", baz(5));
+
+
+            Console.ReadLine();
         }
     }
 }
